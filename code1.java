3dbe861823a68b6870f5b2f0b98ec6b6ffa433b0
laharireddy09 <laharireddy1112@gmail.com>
2023-10-27 12:49:47 -0500

    sathish added

--- code1.java
+++ code1.java
@@ -2,5 +2,7 @@ class code1 {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("how are you");
+
+        System.out.println("sathish");
     }
 }
